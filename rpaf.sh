f7e4e3890bdfa821cb745bf6ecbc886999abeb7d
David Wittman <david@wittman.com>
2011-07-19 03:17:26 -0500

    Fixed suggested Load Balancer IP range

--- rpaf.sh
+++ rpaf.sh
@@ -56,7 +56,7 @@ guess_lb() {
 	if [ ! -d /var/log/${APACHE} ]; then
 		return
 	fi
-	LB_GUESS=$(awk '/^10\.18.\.[^\s]*/ { print $1; exit }' /var/log/httpd/*access?log)
+	LB_GUESS=$(awk '/^10\.18[23]\.[^\s]*/ { print $1; exit }' /var/log/httpd/*access?log)
 	LB_GUESS=${LB_GUESS:-""}
 }
 
