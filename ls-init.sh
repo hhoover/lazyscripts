d6b0997c459d19e79afd2848f1d41fb70213a250
Hart Hoover <hart.hoover@gmail.com>
2011-10-21 09:13:39 -0500

    Updated lsvhost - virtual hosts are easier to read

--- ls-init.sh
+++ ls-init.sh
@@ -373,33 +373,37 @@ if [[ $1 != "" ]]; then
 	read -p "Please enter a domain (no www): " 	domain
 fi
 	if [[ $distro = "Redhat/CentOS" ]]; then
-		cat > /etc/httpd/vhost.d/$domain.conf <<-EOF
-		<VirtualHost *:80>
-		ServerName $domain
-		ServerAlias www.$domain
-		DocumentRoot /var/www/vhosts/$domain
-			<Directory /var/www/vhosts/$domain>
-			AllowOverride All
-			</Directory>
-		CustomLog logs/$domain-access_log common
-		ErrorLog logs/$domain-error_log
-		</VirtualHost>
-		EOF
+
+cat > /etc/httpd/vhost.d/$domain.conf << EOF
+<VirtualHost *:80>
+ServerName $domain
+ServerAlias www.$domain
+DocumentRoot /var/www/vhosts/$domain
+	<Directory /var/www/vhosts/$domain>
+	AllowOverride All
+	</Directory>
+CustomLog logs/$domain-access_log common
+ErrorLog logs/$domain-error_log
+</VirtualHost>
+EOF
+
 		mkdir -p /var/www/vhosts/$domain
 		service httpd restart > /dev/null 2>&1
 	elif [[ $distro = "Ubuntu" ]]; then
-		cat > /etc/apache2/sites-available/$domain <<-EOF
-		<VirtualHost *:80>
-		ServerName $domain
-		ServerAlias www.$domain
-		DocumentRoot /var/www/vhosts/$domain
-			<Directory /var/www/vhosts/$domain>
-			AllowOverride All
-			</Directory>
-		CustomLog /var/log/apache2/$domain-access_log common
-		ErrorLog /var/log/apache2/$domain-error_log
-		</VirtualHost>
-		EOF
+
+cat > /etc/apache2/sites-available/$domain << EOF
+<VirtualHost *:80>
+ServerName $domain
+ServerAlias www.$domain
+DocumentRoot /var/www/vhosts/$domain
+	<Directory /var/www/vhosts/$domain>
+	AllowOverride All
+	</Directory>
+CustomLog /var/log/apache2/$domain-access_log common
+ErrorLog /var/log/apache2/$domain-error_log
+</VirtualHost>
+EOF
+
 		mkdir -p /var/www/vhosts/$domain
 		a2ensite $domain > /dev/null 2>&1
 		service apache2 restart	 > /dev/null 2>&1
