2adc4f64daec64161b698179efe40020973d5b07
Hart Hoover <hart.hoover@rackspace.com>
2012-04-14 11:30:53 -0500

    Getting rid of clippy

--- ls-init.sh
+++ ls-init.sh
@@ -111,18 +111,6 @@ function lsbwprompt() {
 	PS1="[\h \t]-(\w)# "
 }
 
-function lsclippy() {
-# Just for fun. Haters gonna hate.
-cat <<-EOF
-I see you're installing LazyScripts...
-	v
-	╭─╮
-	⌾ ⌾
-	│||
-	╰─╯
-EOF
-}
-
 # ostype - Determine Linux distribution
 function ostype() {
 	if [ -e /etc/redhat-release ]; then
@@ -497,7 +485,6 @@ function lslogin() {
 	lsresize
 	tset -s xterm
 	clear
-	lsclippy
 	lscolors
 	lsinfo
 	lscolorprompt
