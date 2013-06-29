2025f9e9994fbd109d59affed90f9781f46bb899
miroatme <kevink@linux.com>
2013-06-28 20:43:42 -0500

    Thinking forward about SSL on vhost.sh
    Changed localhost.(crt|key) to $domain.(crt|key) to make things easier in the future if SSL is ever desired.

--- modules/vhost.sh
+++ modules/vhost.sh
@@ -66,8 +66,8 @@ function noSSL() {
 		# LogLevel warn
 
 		# SSLEngine on
-		# SSLCertificateFile    /etc/pki/tls/certs/localhost.crt
-		# SSLCertificateKeyFile /etc/pki/tls/private/localhost.key
+		# SSLCertificateFile    /etc/pki/tls/certs/$domain.crt
+		# SSLCertificateKeyFile /etc/pki/tls/private/$domain.key
 		# SSLCertificateChainFile /etc/pki/tls/certs/CA.crt		
 
 		# <FilesMatch "\.(cgi|shtml|phtml|php)$">
